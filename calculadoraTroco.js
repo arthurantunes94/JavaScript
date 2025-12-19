09c3a7678be530630b7e41c52eca6d9aff0413e8
arthurantunes94 <arthurantunes0802@gmail.com>
2025-12-19 17:08:01 -0300

    Exercicio operadores calculadora de troco

---
+++ calculadoraTroco.js
@@ -0,0 +1,5 @@
+console.log('Bem vindo a calculadora de trocos')
+let valorCompra = 17.5
+let valorPago = 20
+let valorTroco = valorPago - valorCompra
+console.log('O valor do troco é:', valorTroco)
